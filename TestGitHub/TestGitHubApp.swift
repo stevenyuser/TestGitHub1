d587d0633a9c7214827b86825b35379ddce1d31b
Steven Yu <stevenyuser@gmail.com>
2022-01-11 11:26:31 -0500

    Initial Commit

---
+++ TestGitHub/TestGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitHubApp.swift
+//  TestGitHub
+//
+//  Created by Steven Yu on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TestGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
